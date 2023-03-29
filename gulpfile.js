d806d8e5762f04874ca375fb9e7409e695889700
François MINET <fr.minet@gmail.Com>
2023-03-29 16:44:28 +0200

    Reloving some gulp problems and config browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,60 @@
-"use strict";
-
 // Load plugins
-const { src, dest }     = require('gulp');
+const { src, dest, watch, series }     = require('gulp');
 const sourcemaps        = require('gulp-sourcemaps');
 const concat            = require('gulp-concat');
 const sass              = require('gulp-sass')(require('sass'));
-const postcss           = require('gulp-postcss');
 const cssnano           = require('gulp-cssnano');
-
-// const autoprefixer      = require('gulp-autoprefixer');
+const autoprefixer      = require('gulp-autoprefixer');
 // const imagemin          = require('gulp-imagemin');
 // const plumber           = require('gulp-plumber');
-// const browserSync       = require('browser-sync').create();
+const sync       = require('browser-sync').create();
 // var reload              = browserSync.reload;
 // const watch             = require('gulp-watch');
 // const rename            = require ('gulp-rename');
 // const uglify            = require('gulp-uglify');
 
 // Sass task
-function sassTask() {
-  return src('./source/sass/**/*.sass')
+function sassTask(cb) {
+  src('./source/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(cssnano()))
-    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(concat('main.min.css'))
     .pipe(sourcemaps.write())
-    .pipe(dest('./build/css/'));
+    .pipe(dest('build/css/'))
+    .pipe(sync.stream());
+  cb();
 }
 
 // JS task
 
 // Images task
 
-// Browser task
+// Watch Task
+function watchTask(cb) {
+  
+}
+
+// BrowserSync task
+function browserSyncTask(cb) {
+  sync.init({
+    server: {
+      baseDir: "./build"
+    }
+  });
+
+  watch('./source/sass/**/**.scss', sassTask);
+  watch("./build/**.html").on('change', sync.reload)
+
+}
+
+// Exports tasks
+exports.css = sassTask;
+exports.sync = browserSyncTask;
+
 
 // Default Gulp task
-exports.default = sassTask
-
+exports.default = series(sassTask,browserSyncTask);
 
 
